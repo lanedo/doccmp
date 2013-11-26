7c7b5468ae309e09c61899e1199d95bbc9518f3f
Pierre-Eric Pelloux-Prayer <pierre-eric@lanedo.com>
2013-11-26 10:21:22 +0100

    Fix table creation script

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,4 +1,4 @@
 #SQL table creations commands:
 
-CREATE TABLE items(pagecount NUMERIC,id TEXT,name TEXT,extension TEXT,PRIMARY KEY(pagecount));
+CREATE TABLE items(pagecount NUMERIC,id TEXT,name TEXT,extension TEXT,PRIMARY KEY(id));
 CREATE TABLE scores(commitsha TEXT,id TEXT,olscore REAL,ollscore REAL,olwscore REAL,details TEXT,version TEXT,path2LO TEXT,PRIMARY KEY(commitsha, id),FOREIGN KEY(id) REFERENCES items(id));
